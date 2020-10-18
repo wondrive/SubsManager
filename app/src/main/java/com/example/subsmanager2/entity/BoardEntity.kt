960360cb85c80421407d2103d7dc9940b6ffa46a
dndbekfrl1 <dndbekfrl1@gmail.com>
2020-10-18 13:33:03 +0900

    Update BoardEntity.kt

--- app/src/main/java/com/example/subsmanager2/entity/BoardEntity.kt
+++ app/src/main/java/com/example/subsmanager2/entity/BoardEntity.kt
@@ -2,17 +2,20 @@ package com.example.subsmanager2.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Entity(tableName = "Board")
 data class BoardEntity(    // ID, 제목, 내용, 글쓴이, 이미지, (게시일, 수정일) => 필요시 추후 추가
     @PrimaryKey(autoGenerate = true)
     var boardId: Long ?= null, // 게시글 고유 ID
     var userId:String,         // 글쓴이
-    //Todo : 좋아요, 별점, 생성 날짜, 아이디
+    //Todo : 좋아요, 별점, 아이디
     var boardTitle:String,     // 제목
     var boardContent:String,   // 내용
     var subFee:String,         // 요금
     var subContents:String,         // 요금
     var usage:String,         // 지속사용
-    var boardImg:String?       // 이미지s
+    var boardCreateDt:String
 )
