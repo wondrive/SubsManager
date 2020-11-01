acbb95b4114a3577a23d3cbb717d24429ff2e336
dndbekfrl1 <dndbekfrl1@gmail.com>
2020-11-02 05:22:30 +0900

    Update SubsListFragment.kt

--- app/src/main/java/com/example/subsmanager2/subs/SubsListFragment.kt
+++ app/src/main/java/com/example/subsmanager2/subs/SubsListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.subsmanager2.subs
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.subsmanager2.R
 import com.example.subsmanager2.dao.SubsDao
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.fragment_mypage.view.*
 import kotlinx.android.synthetic.main.fragment_subs_list.view.*
 
 /**
@@ -17,57 +20,73 @@ import kotlinx.android.synthetic.main.fragment_subs_list.view.*
  */
 class SubsListFragment : Fragment() {
 
-    // DAO
-    val subsDao = SubsDao()
-
+    //현재 로그인한 유저정보를 가져옴
+    var user =""
+    //지출하고 있는 총 구독료
+    var totalFee = 0
     //어댑터 생성
     val subsAdapter = SubsAdapter()
+    //현재 view
+    var rootView:View?=null
+    // DAO
+    val subsDao = SubsDao()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_subs_list, container, false)
-
-        /* 어댑터 초기화*/
-        rootView.list_subs.adapter = subsAdapter
-        rootView.list_subs.layoutManager = LinearLayoutManager(requireContext())
-
+        //fragment_list 인플레이션
+        rootView = inflater.inflate(R.layout.fragment_subs_list, container, false)
         if(subsAdapter != null) {
-            rootView.txt_notice.visibility = View.GONE
+            rootView?.txt_notice?.visibility = View.GONE
         }
+
+        //현재 사용자 정보
+        FirebaseAuth.getInstance().currentUser?.let { it ->
+            user=it.email.toString()
+            Log.d("user adapter:",user)
+        }
+
+        //요금 총합
+        totalFee=0
+            subsDao.firestore?.collection("subs")
+                .orderBy("subsId", Query.Direction.DESCENDING)
+                .whereEqualTo("userId", user.toString())
+                ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    for (snapshot in querySnapshot!!.documents) {
+                        Log.d("fee : ", snapshot.get("fee").toString())
+                        val fee = snapshot.get("fee")
+                        if (fee is String) {
+                            totalFee += fee.toInt()
+                        }
+                    }
+                    rootView?.item_txt_total_fee?.text = "총 구독료: " + totalFee.toString() + "₩"
+                }
+
 
         /* 구독앱 추가 버튼 누르면 */
-        rootView.fab_add_note.setOnClickListener {
+        rootView?.fab_add_note?.setOnClickListener {
             findNavController().navigate(R.id.action_subsListFragment_to_subsWriteFragment)
         }
-        return rootView
+
+        return rootView// 생성한 fragment_list 뷰 반환
+    }//end of onCreateView
+
+    override fun onResume() {
+        super.onResume()
+        /* 어댑터 초기화*/
+        rootView?.list_subs?.adapter = subsAdapter
+        rootView?.list_subs?.layoutManager = LinearLayoutManager(requireContext())
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 여기 주석    @혜린 @진아
-        /*var id: String = ""
-        FirebaseAuth.getInstance().currentUser?.let {
-            //id = it.email?:
-        }
-
-        subsDao.selectSubsList(id).observe(viewLifecycleOwner, androidx.lifecycle.Observer{
-            subsAdapter.subsList = it//어댑터에 변경된 note 전달
-            subsAdapter.notifyDataSetChanged()//어댑터에 변경 공지
-        })*/
-
     }//end of onViewCreated
 
     override fun onStart() {
         super.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        //requireActivity().finish()
-        //activity!!.finish()
     }
 
     override fun onDestroy() {
